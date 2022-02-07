e78acc738537f753a5fb913928a95c6abc24b571
tfdeepnet <deep.hazar@gmail.com>
2022-02-07 10:36:50 +0530

    controlled search

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,22 @@
+import { useState } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Header from './components/Header';
+import Search from './components/Search';
+
 
 function App() {
+
+  const [word, setWord] = useState('');
+
+  const handleSearchSubmit = (e) => {
+    e.preventDefault();
+    console.log('search for an -',word)
+  }
+  
   return (
     <div className="App">
       <Header title="Images gallery!!"/>
+      <Search word={word} setWord={setWord} handleSubmit={handleSearchSubmit}/>
 
     </div>
   );
