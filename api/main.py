216f075c37d375bc33f9844eff38a0ab325d838b
tfdeepnet <deep.hazar@gmail.com>
2022-02-27 22:23:07 +0530

    env variable

--- api/main.py
+++ api/main.py
@@ -1,23 +1,31 @@
 # save this as app.py
-import imp
-from flask import Flask
-from other_module import fn_from_other_module
-
-#fn_from_other_module()
-print(__name__)
+import os
+import requests
+from flask import Flask , request
+from dotenv import load_dotenv
 
 app = Flask(__name__)
 
 #alternative approach
 # def hello():
 #   return "Hello, venv World!"
-
 # app.route("/")(hello)
 
-# main approach
-@app.route("/")
-def hello():
-  return "Hello, venv World!"
+UNSPLASH_URL="https://api.unsplash.com/photos/random"
+UNSPLASH_KEY="ffInDkmie3zoGFuhUU9klJLe4mUJfgi1LNx-wBpIw7Q"
 
+load_dotenv(dotenv_path="./.env.local")
+print(os.environ.get("UNSPLASH_KEY",""))
+
+# main approach
+@app.route("/new-image")
+def new_image():
+  word = request.args.get("query")
+  headers = {"Accept-Version": "v1", "Authorization": "Client-ID " + UNSPLASH_KEY}
+  params = {"query": word}
+  response = requests.get(url=UNSPLASH_URL, headers=headers, params=params)
+
+  data = response.json()
+  return data
 if __name__ == "__main__":
   app.run(host="0.0.0.0", port="5050")
