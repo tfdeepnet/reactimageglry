e68340fe15af8876ae0193cf7a2eb6e62ea2442f
tfdeepnet <deep.hazar@gmail.com>
2022-02-27 10:42:17 +0530

    added flask app

--- api/main.py
+++ api/main.py
@@ -1,9 +1,23 @@
 # save this as app.py
+import imp
 from flask import Flask
+from other_module import fn_from_other_module
 
+#fn_from_other_module()
+print(__name__)
 
 app = Flask(__name__)
 
+#alternative approach
+# def hello():
+#   return "Hello, venv World!"
+
+# app.route("/")(hello)
+
+# main approach
 @app.route("/")
 def hello():
   return "Hello, venv World!"
+
+if __name__ == "__main__":
+  app.run(host="0.0.0.0", port="5050")
